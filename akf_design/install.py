4aa5fd178b5ae1a9b3970375f7702786d22424ce
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-06-03 08:12:36 +0000

    now using login.py from akf_desings.

--- akf_design/install.py
+++ akf_design/install.py
@@ -2,8 +2,8 @@ import frappe
 import os
 # bench --site al-khidmat.com execute akf_design.install.after_install
 def after_install():
-	old_file_path = "/home/frappe/frappe-bench/apps/frappe/frappe/www/login.html"
-	new_file_path = "/home/frappe/frappe-bench/apps/frappe/frappe/www/login_stop.html"
+	old_file_path = "/home/frappe/frappe-alkhidmat/apps/frappe/frappe/www/login.html"
+	new_file_path = "/home/frappe/frappe-alkhidmat/apps/frappe/frappe/www/login_stop.html"
 	try:
 		os.rename(old_file_path, new_file_path)
 		print(f"File renamed from '{old_file_path}' to '{new_file_path}'")
